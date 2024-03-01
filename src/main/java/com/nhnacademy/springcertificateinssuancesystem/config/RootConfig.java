c27b82633640d0e52e1f2528789753bc7f3efd9b
yujinKim <yujinkimvv@gmail.com>
2024-03-01 15:01:44 +0900

    mysql 데이터베이스 연결 설정

--- src/main/java/com/nhnacademy/springcertificateinssuancesystem/config/RootConfig.java
+++ src/main/java/com/nhnacademy/springcertificateinssuancesystem/config/RootConfig.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.springcertificateinssuancesystem.config;
 
 import com.nhnacademy.springcertificateinssuancesystem.Base;
+import com.nhnacademy.springcertificateinssuancesystem.properties.DatabaseProperties;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,13 +11,31 @@ import org.springframework.stereotype.Controller;
 
 import javax.sql.DataSource;
 
+@RequiredArgsConstructor
 @Configuration
 @ComponentScan(basePackageClasses = Base.class,
         excludeFilters = @ComponentScan.Filter(Controller.class))
 public class RootConfig {
+
+    private final DatabaseProperties databaseProperties;
+
     @Bean
     public DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        return dataSource;
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName(databaseProperties.getDriverClassName());
+        basicDataSource.setUrl(databaseProperties.getUrl());
+        basicDataSource.setUsername(databaseProperties.getUsername());
+        basicDataSource.setPassword(databaseProperties.getPassword());
+        basicDataSource.setInitialSize(databaseProperties.getInitialSize());
+        basicDataSource.setMaxTotal(databaseProperties.getMaxTotal());
+        basicDataSource.setMinIdle(databaseProperties.getMinIdle());
+        basicDataSource.setMaxIdle(databaseProperties.getMaxIdle());
+        basicDataSource.setTestOnBorrow(databaseProperties.isTestOnBorrow());
+        if(databaseProperties.isTestOnBorrow()) {
+            basicDataSource.setValidationQuery(basicDataSource.getValidationQuery());
+        }
+        return basicDataSource;
     }
+
 }
+
--- src/main/java/com/nhnacademy/springcertificateinssuancesystem/config/WebConfig.java
+++ src/main/java/com/nhnacademy/springcertificateinssuancesystem/config/WebConfig.java
@@ -1,13 +1,18 @@
 package com.nhnacademy.springcertificateinssuancesystem.config;
 
 import com.nhnacademy.springcertificateinssuancesystem.controller.ControllerBase;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackageClasses = ControllerBase.class)
 public class WebConfig implements WebMvcConfigurer {
+
 }
