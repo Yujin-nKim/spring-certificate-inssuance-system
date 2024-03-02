1958a748bf9183f0a54ed639b818e63f00f239c1
yujinKim <yujinkimvv@gmail.com>
2024-03-03 00:58:41 +0900

    Resident Entity test 코드 오류 해결

--- src/test/java/EntityTest.java
+++ src/test/java/EntityTest.java
@@ -4,8 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.nhnacademy.springcertificateinssuancesystem.config.RootConfig;
 import com.nhnacademy.springcertificateinssuancesystem.config.WebConfig;
+import com.nhnacademy.springcertificateinssuancesystem.entity.BirthDeathReportResident;
 import com.nhnacademy.springcertificateinssuancesystem.entity.Resident;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -32,14 +34,15 @@ public class EntityTest {
     @DisplayName("Resident Entity test")
     void testResidentEntity() {
         Resident resident = entityManager.find(Resident.class, 1);
-        assertThat(resident.getName().toString().equals("남길동"));
-        assertThat(resident.getResidentRegistrationNumber().toString().equals("130914-1234561"));
-        assertThat(resident.getGenderCode().toString().equals("남"));
-        assertThat(resident.getBirthDate().toString().equals("19130914072200"));
-        assertThat(resident.getBirthPlaceCode().toString().equals("자택"));
-        assertThat(resident.getRegistrationBaseAddress().toString().equals("경기도 성남시 분당구 대왕판교로645번길"));
-        assertThat(resident.getDeathDate().toString().equals("20210429090300"));
-        assertThat(resident.getDeathPlaceCode().toString().equals("주택"));
-        assertThat(resident.getDeathPlaceAddress().toString().equals("강원도 고성군 금강산로 290번길"));
+
+        assertThat(resident.getName()).isEqualTo("남길동");
+        assertThat(resident.getResidentRegistrationNumber()).isEqualTo("130914-1234561");
+        assertThat(resident.getGenderCode()).isEqualTo("남");
+        assertThat(resident.getBirthDate().getYear()).isEqualTo(1913);
+        assertThat(resident.getBirthPlaceCode()).isEqualTo("자택");
+        assertThat(resident.getRegistrationBaseAddress()).isEqualTo("경기도 성남시 분당구 대왕판교로645번길");
+        assertThat(resident.getDeathDate().getYear()).isEqualTo(2021);
+        assertThat(resident.getDeathPlaceCode()).isEqualTo("주택");
+        assertThat(resident.getDeathPlaceAddress()).isEqualTo("강원도 고성군 금강산로 290번길");
     }
 }
